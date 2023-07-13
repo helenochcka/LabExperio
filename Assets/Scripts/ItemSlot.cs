6f6f8e0440bdcb5ad526c33d96fbeeae63b36d5b
wwmmario <mariavikhliaeva@gmail.com>
2023-07-13 17:35:39 +0300

    Demo Inventory

---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
