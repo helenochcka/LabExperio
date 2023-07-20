410d036c94b490ff554a7e6a079156524a79afde
wwmmario <mariavikhliaeva@gmail.com>
2023-07-20 20:05:40 +0300

    edit inventory + little animations

--- Assets/Scripts/Inventory/ItemClass.cs
+++ Assets/Scripts/Inventory/ItemClass.cs
@@ -7,7 +7,6 @@ public abstract class ItemClass : ScriptableObject
     [Header("Item")]
     public string itemName;
     public Sprite itemIcon;
-    public bool isStackable = true;
 
     public abstract ItemClass GetItem();
     public abstract ToolClass GetTool();
