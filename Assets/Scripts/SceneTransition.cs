1d966f04b44c10e89bc9c6cbb02ee31673bf1654
wwmmario <mariavikhliaeva@gmail.com>
2023-07-21 12:15:54 +0300

    edit button

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+   public int sceneNumber;
+   public void Transition()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+  
+}
